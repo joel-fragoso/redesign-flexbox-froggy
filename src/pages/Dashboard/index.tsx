4fa0ed27a794a28c57ae178f093c540ceb330b3a
Joel Fragoso <joelfragoso85@icloud.com>
2020-07-08 15:17:38 -0300

    feat: creating dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: FunctionComponent = () => {
+  return <Container />;
+};
+
+export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+
+  grid-template-rows: 50px 48px auto 50px;
+  grid-template-columns: 240px auto;
+
+  grid-grid-template-areas: 'Header Header' 'Info Navigation' 'Description Code'
+    'Instructions Code' 'Instructions Footer';
+
+  height: 100vh;
+`;
